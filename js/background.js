7d4994b9bddbbd1c45715502e115421316dad88e
huangtian <tian.huang@mtime.com>
2017-07-31 10:00:50 +0800

    first commit

---
+++ js/background.js
@@ -0,0 +1,147 @@
+var Background = (function() {
+	// variables ----------------------------------------------------------------
+	var _this = {};
+
+	var _requestFilter = {
+		urls: [
+			'https://www.google.com/*',
+			'https://www.baidu.com/*',
+			'http://cn.bing.com/*'
+		]
+	};
+	var _ua = 'Mozilla/5.0 (iPad; CPU OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1';
+
+	// initialize ---------------------------------------------------------------
+	_this.init = function() {
+		// receive post messages from "inject.js" and any iframes
+		chrome.extension.onRequest.addListener(onPostMessage);
+
+		// manage when a user change tabs
+		// chrome.tabs.onActivated.addListener(onTabActivated);
+
+		//linstening hotkey press events
+		chrome.commands.onCommand.addListener(onHotkeyPressed);
+
+		//set x-frame-options for load google into iframe
+		chrome.webRequest.onHeadersReceived.addListener(setResponseHeader, _requestFilter, ["blocking", "responseHeaders"]);
+
+		//rewrite  user agent before request
+		// chrome.webRequest.onBeforeSendHeaders.addListener(setUserAgentBeforeRequest, _requestFilter, ['requestHeaders', 'blocking']);
+	}
+	// private functions --------------------------------------------------------
+	function upateCurrentTab() {};
+
+	function processMessage(request) {
+		// process the request
+		switch (request.message) {
+			case 'search-button-clicked':
+				message_onSearchButtonClicked(request.data);
+				break;
+			case 'all-iframes-loaded':
+				message_allIframesLoaded(request.data);
+				break;
+		}
+	};
+
+	// start up easy search
+	function startEasySearch() {
+		_this.tell('start_up_easy_search', {});
+	}
+
+	function stopEasySearch() {
+		_this.tell('stop_easy_search', {});
+	}
+
+	//set user agent before request
+	function setUserAgentBeforeRequest(d) {
+		var headers = d.requestHeaders;
+		for (var i = 0; i < headers.length; i++) {
+			if (headers[i].name.toLowerCase() == "user-agent") {
+				headers[i].value = _ua;
+				break;
+			}
+		}
+		return {
+			requestHeaders: headers
+		};
+	}
+
+	//set x-frame-options for load google into iframe
+	function setResponseHeader(details) {
+		for (var i = 0; i < details.responseHeaders.length; ++i) {
+			if (details.responseHeaders[i].name.toLowerCase() == 'x-frame-options') {
+				details.responseHeaders.splice(i, 1);
+				return {
+					responseHeaders: details.responseHeaders
+				};
+			}
+		}
+	}
+
+	// events -------------------------------------------------------------------
+	function onPostMessage(request, sender, sendResponse) {
+		if (!request.message) return;
+
+		// if it has a "view", it resends the message to all the frames in the current tab
+		if (request.data.view) {
+			_this.tell(request.message, request.data);
+			return;
+		}
+
+		processMessage(request);
+	};
+
+	function onTabActivated() {
+		upateCurrentTab();
+	};
+
+	function onHotkeyPressed(command) {
+		switch (command) {
+			case 'start_up_easy_search':
+				startEasySearch();
+				break;
+			case 'stop_easy_search':
+				stopEasySearch();
+				break;
+			default:
+				console.log(command);
+				break;
+		}
+
+	};
+
+	// messages -----------------------------------------------------------------
+	function message_onSearchButtonClicked(data) {
+		console.log('background receive messages: ..begin search keyword== ' + data.keyword);
+		return;
+		// create new tab for show search result
+		chrome.tabs.create({
+			url: "https://www.google.com/search?q=" + data.keyword
+		});
+	};
+
+	function message_allIframesLoaded(data) {
+		upateCurrentTab();
+	};
+
+	// public functions ---------------------------------------------------------
+	_this.tell = function(message, data) {
+		var data = data || {};
+
+		// find the current tab and send a message to "inject.js" and all the iframes
+		chrome.tabs.getSelected(null, function(tab) {
+			if (!tab) return;
+
+			chrome.tabs.sendMessage(tab.id, {
+				message: message,
+				data: data
+			});
+		});
+	};
+
+	return _this;
+}());
+
+window.addEventListener("load", function() {
+	Background.init();
+}, false);
---
+++ js/iframe/search.js
@@ -0,0 +1,99 @@
+var Search = (function() {
+	// variables ----------------------------------------------------------------
+	var _this = {},
+		_iframe = null;
+
+	// initialize ---------------------------------------------------------------
+	_this.init = function() {
+		_iframe = new IframeManager();
+		_iframe.setListener(onMessage);
+
+		$("#keyword").focus();
+		$('#search-button').on('click', onSearchButtonClick);
+		$('#close-button').on('click', onCloseButtonClick);
+
+		$(document).keyup(onEnterKeyUp);
+	};
+
+	// private functions --------------------------------------------------------
+	function loadGoogleSearch(keyword) {
+		debugger;
+		iframe = $('<iframe />', {
+			id: 'google',
+			src: "https://www.google.com/search?q=" + keyword,
+			scrolling: true
+		});
+		$('#left-search').append(iframe);
+	}
+
+	function loadBaiduSearch(keyword) {
+		iframe = $('<iframe />', {
+			id: 'baidu',
+			src: "https://www.baidu.com/s?wd=" + keyword,
+			scrolling: true
+		});
+		$('#right-search').append(iframe);
+	}
+
+	function loadBingSearch(keyword) {
+		iframe = $('<iframe />', {
+			id: 'bing',
+			src: "http://cn.bing.com/search?q=" + keyword,
+			scrolling: true
+		});
+		$('#right-search').append(iframe);
+	}
+
+
+	// events -------------------------------------------------------------------
+	function onMessage(request) {
+		switch (request.message) {
+			case 'open-search':
+				message_onOpenSearch(request.data);
+				break;
+		}
+	};
+
+	function onEnterKeyUp(e) {
+		var curKey = e.which;
+		if (curKey == 13) {
+			onSearchButtonClick();
+		}
+	}
+
+	// start search
+	function onSearchButtonClick(event) {
+		var keyword = $('#keyword').val() || '';
+		if (keyword.length == 0) return;
+
+		loadGoogleSearch(keyword);
+		// loadBaiduSearch(keyword);
+		loadBingSearch(keyword);
+
+		$('#search-box').hide();
+		// hide iframe and delete app-container
+		_iframe.tell('search-button-clicked', {
+			keyword: keyword
+		});
+	};
+
+	// logout app and close  search view
+	function onCloseButtonClick(event) {
+		//delete app-container
+		_iframe.tell('close-app-view', {});
+	};
+
+	// messages -----------------------------------------------------------------
+	function message_onOpenSearch(data) {
+		$('.page-title').html(data.title);
+	};
+
+
+	// public functions ---------------------------------------------------------
+
+	return _this;
+}());
+
+document.addEventListener("DOMContentLoaded", function() {
+	new Search.init();
+}, false);
